3ced7e5ed74fa828439bb8bb156eeca679c24a0d
eeennsu <diso592@naver.com>
2023-01-21 19:47:24 +0900

    스타일링

---
+++ client/src/components/styled/eunsu/StyledComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const ParantDiv = styled.section`
+    min-height: calc(100vh - 300px);
+    padding-top: 65px;  
+    background-color: grey;
+`;
---
+++ client/src/components/views/ManagerPage/ManagerPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ManagerPage = () => {
+    return (
+        // 예시 스타일
+        <section style={{ display: 'flex', width: '85%', background: 'brown', margin: '2rem auto', height: '80vh' }}>
+			ManagerPage
+		</section>
+    );
+};
+
+export default ManagerPage;
