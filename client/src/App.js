f7add7e0384c7fa611915e7657b584ac89fb2443
eeennsu <diso592@naver.com>
2023-01-21 18:09:28 +0900

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
 import React      from 'react';
-import Main       from './components/Main/Main';
-import Intro      from './components/Intro/Intro';
+import Main       from './components/views/Main/Main';
+import Intro      from './components/views/Intro/Intro';
 import Active     from './components/views/Active/Active';
-import Contest    from './components/Contest/Contest';
-import Navbar     from './components/Navbar/Navbar';
-import Footer     from './components/Footer/Footer';
+import Contest    from './components/views/Contest/Contest';
+import Navbar     from './components/views/Navbar/Navbar';
+import Footer     from './components/views/Footer/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
     return (
         <BrowserRouter>
-          <Navbar />                                                  {/* 네비게이션 바  */}
-          <Routes>
-            <Route exact path='/' element={<Main />}/>                {/*  메인 페이지   */}
-            <Route path='/intro' element={<Intro />}/>                {/*  소개 페이지   */}
-            <Route path='/active' element={<Active />}/>              {/*  활동 페이지   */}
-            <Route path='/contest' element={<Contest />}/>            {/*  공모전 페이지 */}
-          </Routes>   
-          <Footer />                                                  {/*  홈페이지 정보 */}
+			<Navbar />                                                  {/* 네비게이션 바  */}
+			<Routes>
+				<Route exact path='/' element={<Main />}/>                {/*  메인 페이지   */}
+				<Route path='/intro' element={<Intro />}/>                {/*  소개 페이지   */}
+				<Route path='/active' element={<Active />}/>              {/*  활동 페이지   */}
+				<Route path='/contest' element={<Contest />}/>            {/*  공모전 페이지 */}
+			</Routes>   
+			<Footer />                                                  {/*  홈페이지 정보 */}
         </BrowserRouter>
     );
 }
