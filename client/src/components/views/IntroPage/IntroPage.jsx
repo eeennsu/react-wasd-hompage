6e5991f8050aa153b1320f629030fbe1f24248c9
eeennsu <diso592@naver.com>
2023-02-18 12:09:21 +0900

    인트로 페이지 수정

--- client/src/components/views/IntroPage/IntroPage.jsx
+++ client/src/components/views/IntroPage/IntroPage.jsx
@@ -6,20 +6,28 @@ import { ParentDiv }                  from '../../styled/eunsu/StyledComponent';
 import { useNavigate }                from 'react-router-dom';
 import { CLUB_INFO }                  from '../apiConfig';
 
+// 서버 경롷
+const endPoints = [
+    `${CLUB_INFO}/동아리소개`,
+    `${CLUB_INFO}/동아리비전`,
+    `${CLUB_INFO}/동아리교수`
+];
+
+// 카드 제목
+const titles = [        
+    'Introduce',
+    'Vision',
+    'Instructor'        
+];
+
 const IntroPage = () => {
    
-    const [clubInfos, setClubInfos] = useState([]);
+    const [clubInfos, setClubInfos] = useState(Array.from({ length: titles.length }));
     const [isLoading, setIsLoading] = useState(true);
-    const navigate = useNavigate();
+    const navigate = useNavigate();  
 
-    // 서버에서 가져오기
-    useEffect(() => {
-        const endPoints = [
-            `${CLUB_INFO}/동아리소개`,
-            `${CLUB_INFO}/동아리비전`,
-            `${CLUB_INFO}/동아리교수`
-        ];    
-
+    // 최초 접속시 서버에서 가져오기
+    useEffect(() => {       
         axios.all(endPoints.map(endPoint => axios.get(endPoint)))
             .then(axios.spread((res1, res2, res3) => {
                 const resIntroduce  = res1.data;
@@ -27,31 +35,25 @@ const IntroPage = () => {
                 const resInstructor = res3.data;
 
                 if (resIntroduce.suc && resVision.suc && resInstructor.suc) {
-                    console.log('data get success!');
-                    
+                    console.log('data get success!');                   
+
                     const results = [
-                        {
-                            title: <span>A Club<br/>Introduce</span>,
-                            content: resIntroduce.result.info_content
-                        },
-                        {
-                            title: <span>A Club<br/>Vision</span>,
-                            content: resVision.result.info_content
-                        },
-                        {
-                            title: <span>A Club<br/>Instructor</span>,
-                            content: resInstructor.result.info_content
-                        }
+                        resIntroduce.result.info_content,
+                        resVision.result.info_content,
+                        resInstructor.result.info_content
                     ];
 
-                    setClubInfos([...results]);
-                    setIsLoading(false);
+                    setClubInfos([...results]);                  
+                    setIsLoading(false);                    
                 } else {
                     message.error('data get failed..');
                     navigate('/');
                 }
             }))
-            .catch((e) => message.error('error...'));
+            .catch((e) => {
+                message.error('server error...')
+                console.log(e);              
+            });
     }, []);
 
     return (
@@ -59,22 +61,16 @@ const IntroPage = () => {
             <section className='intro_parent'>
                 <div className='intro_cards_section row'>
                     {
-                        isLoading ? 
-
-                        <Spin tip='Loading' size='large'/>
-
-                        :
-
-                        clubInfos.map(({ title, content }) => (                          
-                            <div className="col-md-4 mb-4 mb-md-0" key={`info-${title}`}>
+                        clubInfos.map((info, i) => (
+                            <div className="col-md-4 mb-4 mb-md-0" key={`info-${titles[i]}}`}>
                                 <InfoCard   
-                                        title={title}                                  
-                                        content={content}
-                                        isLoading={isLoading}                              
+                                    title={titles[i]}                                  
+                                    content={info}
+                                    isLoading={isLoading}                              
                                 />                                                                                      
                             </div>
                         ))
-                    }                   
+                    }
                 </div>
             </section>
         </ParentDiv>    
--- client/src/components/views/ManagerPage/ManagerPage.jsx
+++ client/src/components/views/ManagerPage/ManagerPage.jsx
@@ -1,32 +1,164 @@
 import { MDBIcon } from 'mdb-react-ui-kit';
-import { Button, Form, Input } from 'antd';
-import React, { useCallback } from 'react';
+import { Button, Input as antdInput, Form as antdForm } from 'antd';
+import React from 'react';
 import { ParentDiv } from '../../styled/eunsu/StyledComponent';
-const { Item } 	= Form;
+import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
+
+const Container = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: center;
+`;
+
+const Form = styled(antdForm)`
+	width: 785px;
+	height: 471px;
+	padding: 25px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: #fff;
+	border-radius: 10px;
+`;
+
+const LeftBox = styled.div`
+	border-right: 1px solid black;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const UserInfos = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+const RightBox = styled.div`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Label = styled.label`
+	width: 307px;	
+	margin-bottom: ${({ mb }) => mb && `${mb}px`};
+`;
+
+const TagName = styled.span`
+	font-size: 35px;
+	margin-right: 15px;
+	font-weight: 500;
+`;
+
+const Input = styled.input`
+	width: 70%;
+	outline: none;
+	border: none;
+	background-color: transparent;
+	font-size: 35px;
+	color: black;
+
+	&:-webkit-autofill,
+  	&:-webkit-autofill:hover,
+  	&:-webkit-autofill:focus,
+  	&:-webkit-autofill:active {
+		transition: background-color 5000s ease-in-out 0s;
+		-webkit-transition: background-color 9999s ease-out;
+		-webkit-box-shadow: 0 0 0px 1000px white inset !important;
+		box-shadow: 0 0 0px white inset !important;
+		-webkit-text-fill-color: black !important;
+		font-size: 35px;
+  	}
+`;
+
+const InputBox = styled.div`
+	width: 100%;
+	border-bottom: 1px solid black;	
+	padding: 0;
+	margin: 0;
+	height: 55px;
+`;
+
+const ErrorMsg = styled.p`	
+	height: 10px;	
+	padding: 5px;
+
+	span {
+		color: red;
+		font-size: 14px;
+		font-weight: bold;
+	}
+`;
+
+const Login = styled.button`
+	width: 300px;
+	height: 49px;
+	background-color: #9DC78E;
+	border-radius: 10px;
+	border: none;
+	font-weight: bolder;	
+	color: black;
+	font-family: Noto;
+	font-size: 20px;
+	transition: 0.2s ease-in-out;
+
+	&:hover {
+		background-color: #b4e0a4;
+	}
+`;
 
 const ManagerPage = () => {
 
-	const onFinish = (values) => {
-		console.log(values);
+	const { register, handleSubmit, formState: { errors } } = useForm({
+		shouldFocusError: true,
+	});
+
+	const onSubmit = (data) => {
+		console.log(JSON.stringify(data));
 	};
 
 	return (
-		<ParentDiv>
-			<div id='form_parent' className='d-flex justify-content-center align-items-center'>
-				<Form name='normal_login' className='login-form' initialValues={{ remember: true }}	onFinish={onFinish} > 
-					<Item name='ManagerID' rules={[{ required: true, message: 'Input Manager ID' }]}>
-						<Input prefix={<MDBIcon fas icon='user-alt' />} placeholder='Manager ID' />
-					</Item>
-					<Item name='ManagerPassword' rules={[{ required: true, message: 'Input Manager Password' }]} >
-						<Input prefix={<MDBIcon fas icon='unlock-alt' />} type='password' placeholder='Manager Password' />	
-					</Item>
-					<Item className='text-center'>
-						<Button type='primary' htmlType='submit' className='login-form-button'>
-							Log	in
-						</Button>			
-					</Item>
+		<ParentDiv flex justifyCenter alignCenter>
+			<Container>
+				<Form onFinish={handleSubmit(onSubmit)}>
+					<LeftBox>	
+						<UserInfos>
+							<Label mb={26}>
+								<InputBox>
+									<TagName>ID</TagName>
+									<Input type='text' {...register('id', { required: true })}/>
+								</InputBox>
+								<ErrorMsg>
+									{
+										errors.id?.type === 'required' && <span>ID is required</span>
+									}	
+								</ErrorMsg>											
+							</Label>								
+							<Label>
+								<InputBox>
+									<TagName>PW</TagName>
+									<Input type='password' {...register('pw', { required: true })}/>
+								</InputBox>	
+								<ErrorMsg>
+									{
+										errors.pw?.type === 'required' && <span>Password is required</span>
+									}
+								</ErrorMsg>								
+							</Label>
+						</UserInfos>				
+					</LeftBox>
+					<RightBox>
+						<Login>
+							로그인
+						</Login>
+					</RightBox>
 				</Form>
-			</div>	
+			</Container>
 		</ParentDiv>		
 	);
 };
